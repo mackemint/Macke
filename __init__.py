4861e576b41f67a7ab033ca49984052f8d4f74e4
mackemint <markus.kaczmarek@gmail.com>
2013-12-19 00:24:03 +0100

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# DJNSM / @djnsm / djnsm.com -> datamafia.com
+# Free to use, please refer to the readme.py file for license , info, and the uncompromised enlightenment
+
+from Macke import Macke
+
+def create_instance(c_instance):
+    return Macke(c_instance)
